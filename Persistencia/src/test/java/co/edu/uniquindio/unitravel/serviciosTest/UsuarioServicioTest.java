01ee2610ad7153aa2ab1a6a59df7bbaa0975a3d9
Nicolox <nmurilloc@uqvirtual.edu.co>
2022-05-09 18:00:15 -0500

    Respaldo para hacer la descarga de la entrega uno

--- Persistencia/src/test/java/co/edu/uniquindio/unitravel/serviciosTest/UsuarioServicioTest.java
+++ Persistencia/src/test/java/co/edu/uniquindio/unitravel/serviciosTest/UsuarioServicioTest.java
@@ -1,8 +1,11 @@
 package co.edu.uniquindio.unitravel.serviciosTest;
 
+import co.edu.uniquindio.unitravel.entidades.Usuario;
+import co.edu.uniquindio.unitravel.repositorios.UsuarioRepo;
 import co.edu.uniquindio.unitravel.servicios.EmailService;
 import co.edu.uniquindio.unitravel.servicios.UsuarioServicio;
 import co.edu.uniquindio.unitravel.servicios.UsuarioServicioImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +19,8 @@ public class UsuarioServicioTest {
 
     @Autowired
     private UsuarioServicio usuarioServicio;
+    @Autowired
+    private UsuarioRepo usuarioRepo;
 
     @Autowired
     private EmailService emailService;
@@ -27,7 +32,24 @@ public class UsuarioServicioTest {
 
     }
 
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void registrarUsuarioTest() throws Exception {
+        Usuario u = new Usuario("6", "Nicolas", "nicolas@email.com", "pass");
+        Usuario  usuarioRegistrado = usuarioServicio.registrarUsuario(u);
 
+        Assertions.assertNotNull(usuarioRegistrado);
+
+    }
+
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void loginTest() throws Exception {
+        Usuario u = usuarioServicio.login("a@mail.com","pass1");
+
+
+
+    }
 
 
 
